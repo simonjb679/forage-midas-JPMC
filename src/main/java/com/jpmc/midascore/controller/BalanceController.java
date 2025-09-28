0326603567b898c203e81bb4029e4854a3e5dadd
Simon Benitez <simonjb679@gmail.com>
2025-09-28 16:30:44 -0400

    Task 5: Added REST API for balance queries

---
+++ src/main/java/com/jpmc/midascore/controller/BalanceController.java
@@ -0,0 +1,26 @@
+package com.jpmc.midascore.controller;
+
+import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Balance;
+import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BalanceController {
+    
+    @Autowired
+    private UserRepository userRepository;
+    
+    @GetMapping("/balance")
+    public Balance getBalance(@RequestParam Long userId) {
+        UserRecord user = userRepository.findById(userId.longValue());
+        if (user != null) {
+            return new Balance(user.getBalance());
+        } else {
+            return new Balance(0.0f);
+        }
+    }
+}
